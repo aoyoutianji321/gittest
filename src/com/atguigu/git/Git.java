87599530b5e348522e9b393614ccaede44403d91
tom_global <tom_global@zhaoyin.com>
2021-07-18 18:08:37 +0800

    master commit2

--- src/com/atguigu/git/Git.java
+++ src/com/atguigu/git/Git.java
@@ -6,5 +6,7 @@ public class Git {
         System.out.println("Hello git22!!");
         System.out.println("Hello git33!!");
         System.out.println("Hello git4!!");
+        System.out.println("Hello git4!!");
+        System.out.println("master test!");
     }
 }
